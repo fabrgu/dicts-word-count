e8a76a62d200c9941a962c610200bc7190f13a71
Your Name <you@example.com>
2020-02-03 10:03:47 -0800

    implemented new function with Counter

--- wordcount.py
+++ wordcount.py
@@ -1,5 +1,6 @@
 # put your code here.
 import sys
+from collections import Counter
 
 
 def count_words(file_name):
@@ -16,5 +17,19 @@ def count_words(file_name):
         print(f"{word} {word_dict[word]}")
 
 
+def count_words_counter(file_name):
+    file = open(file_name)
+    word_counter = Counter()
+    for line in file:
+        line = line.strip()
+        words = line.split(" ")
+        for word in words:
+            word_counter[word] = word_counter.get(word, 0) + 1
+                
+    file.close()
+    for word in word_counter:
+        print(f"{word} {word_counter[word]}")
+
+
 file_name = sys.argv[1]
-count_words(file_name)
+count_words_counter(file_name)
