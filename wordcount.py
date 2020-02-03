19436f1878aafe6ba05b173c61501456a64de775
Your Name <you@example.com>
2020-02-03 09:21:45 -0800

    finished count_words function

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,16 @@
 # put your code here.
+def count_words(file_name):
+    file = open(file_name)
+    word_dict = {}
+    for line in file:
+        line = line.strip()
+        words = line.split(" ")
+        for word in words:
+            word_dict[word] = word_dict.get(word, 0) + 1
+
+    file.close()
+    for word in word_dict:
+        print(f"{word} {word_dict[word]}")
+
+
+count_words("twain.txt")
