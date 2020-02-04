9ecbaa911b9937d968cbd412dfe42e874dfb306a
Your Name <you@example.com>
2020-02-04 09:32:29 -0800

    added word_count sorted

--- wordcount.py
+++ wordcount.py
@@ -31,5 +31,21 @@ def count_words_counter(file_name):
         print(f"{word} {word_counter[word]}")
 
 
+def count_words_sorted(file_name):
+    file = open(file_name)
+    word_dict = {}
+    for line in file:
+        line = line.strip()
+        words = line.split(" ")
+        for word in words:
+            word_dict[word] = word_dict.get(word, 0) + 1
+
+    file.close()
+    word_keys = list(word_dict.keys())
+    word_keys.sort(key=str.lower)
+    for word_key in word_keys:
+        print(f"{word_key} {word_dict[word_key]}")
+
+
 file_name = sys.argv[1]
-count_words_counter(file_name)
+count_words_sorted(file_name)
